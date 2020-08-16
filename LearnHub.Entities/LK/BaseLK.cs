3999a377b9d4d2a39a089adbe247d170cc7262c7
Amsaad <ashoooh@gmail.com>
2020-08-16 12:08:00 +0300

    Adding lookup tables

---
+++ LearnHub.Entities/LK/BaseLK.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnHub.Entities
+{
+    class BaseLK : BaseEntity
+    {
+        [Required, StringLength(maximumLength: 250, MinimumLength = 10)]
+        public string Name { get; set; }
+    }
+}
