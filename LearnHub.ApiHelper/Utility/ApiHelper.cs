1f912cdc7ccc3e782497e61a1e44e916ed983131
amsaad <ashraf_sa3d1984@hotmail.com>
2020-08-21 22:26:48 +0300

    - Api helper initiation

---
+++ LearnHub.ApiHelper/Utility/ApiHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnHub.ApiHelper.Utility
+{
+    class ApiHelper
+    {
+        public static HttpClient ApiClient;
+        private static string APIUrl => ConfigurationManager.AppSettings["APIUrl"];
+        public ApiHelper()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.BaseAddress = new Uri(APIUrl);
+        }
+
+        public static async Task<HttpContent> Get(string Controller, string Action, bool allowAnonymous = true, params object[] inputs)
+        {
+
+            return null;
+        }
+    }
+}
